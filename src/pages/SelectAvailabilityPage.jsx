2d87e8cc8aa1bcb83b66298b513d660c2d57cb5e
Jazz Orpheus <redgate888@hotmail.com>
2025-09-13 20:44:45 +0100

    fix instruction on availability page

--- src/pages/SelectAvailabilityPage.jsx
+++ src/pages/SelectAvailabilityPage.jsx
@@ -27,9 +27,9 @@ export default function SelectAvailabilityPage({
     <div className="min-h-screen bg-gray-50">
       {/* HEADER */}
       <PageHeader title="Select Your Availability">
-        {/* Please select your availability from the timeslots below. You don't have */}
-        to be 100% certain you can make it to all the slots you select — we just
-        want an idea of times people tend to be available.
+        {/* Please select your availability from the timeslots below.  */}
+        You don't have to be 100% certain you can make it to all the slots you
+        select — we just want an idea of times people tend to be available.
       </PageHeader>
 
       {/* MAIN CONTENT */}
