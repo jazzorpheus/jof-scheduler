789597773d862f288dacea75aacbb0c0ded7df8f
Jazz Orpheus <redgate888@hotmail.com>
2025-09-14 20:12:12 +0100

    add comments to new TanStack / routes files

--- src/routes/create.jsx
+++ src/routes/create.jsx
@@ -1,9 +1,12 @@
+// TanStack Router
 import { createFileRoute } from "@tanstack/react-router";
 
+// Create route
 export const Route = createFileRoute("/create")({
   component: CreateEvent,
 });
 
+// Route Component
 function CreateEvent() {
   return <div className="p-2">Hello from Create Event!</div>;
 }
--- src/routes/plan.jsx
+++ src/routes/plan.jsx
@@ -1,9 +1,12 @@
+// TanStack Router
 import { createFileRoute } from "@tanstack/react-router";
 
+// Create route
 export const Route = createFileRoute("/plan")({
   component: PlanMatch,
 });
 
+// Route component
 function PlanMatch() {
   return <div className="p-2">Hello from Plan Match!</div>;
 }
